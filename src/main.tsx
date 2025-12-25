ca77d5e22b2a718d07fe6a69fc5d324f4b7a29ee
Icyy <adityaauty9@gmail.com>
2025-12-25 17:56:53 +0530

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,10 @@ import './index.css';
 import { inject } from '@vercel/analytics';
 
 // Call the function as early as possible in your application lifecycle
-inject();
+inject({
+  debug: true, // This will log events to the console even on localhost
+  mode: 'development' // Forces the script to run
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
